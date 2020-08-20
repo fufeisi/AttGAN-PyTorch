6680a7b78a63d031038675daf3442a923733a9e0
fufeisi <55070756+fufeisi@users.noreply.github.com>
2020-08-20 15:27:55 -0400

    Create model.py

---
+++ on_manifold/model.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from nn import LinearBlock, Conv2dBlock, ConvTranspose2dBlock
+from torchsummary import summary
+
+
+class Classifier(nn.Module):
+    # No instancenorm in fcs in source code, which is different from paper.
+    def __init__(self, dim=64, norm_fn='instancenorm', acti_fn='lrelu',
+                 fc_dim=1024, fc_norm_fn='none', fc_acti_fn='lrelu', n_layers=5, img_size=128, MAX_DIM = 64 * 16):
+        super(Classifier, self).__init__()
+        self.f_size = img_size // 2 ** n_layers
+
+        layers = []
+        n_in = 3
+        for i in range(n_layers):
+            n_out = min(dim * 2 ** i, MAX_DIM)
+            layers += [Conv2dBlock(
+                n_in, n_out, (4, 4), stride=2, padding=1, norm_fn=norm_fn, acti_fn=acti_fn
+            )]
+            n_in = n_out
+        self.conv = nn.Sequential(*layers)
+        self.fc_cls = nn.Sequential(
+            LinearBlock(1024 * self.f_size * self.f_size, fc_dim, fc_norm_fn, fc_acti_fn),
+            LinearBlock(fc_dim, 13, 'none', 'none')
+        )
+
+    def forward(self, x):
+        h = self.conv(x)
+        h = h.view(h.size(0), -1)
+        return self.fc_cls(h)
+
+
+classifier = Classifier()
+summary(classifier, (3, 128, 128))
